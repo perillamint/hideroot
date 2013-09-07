a1837e509f93d77072a9541256a543470c640a42
maneulyori <maneulyori@gmail.com>
2013-09-07 22:08:09 +0900

    fix indents & add hooking routine for sys_open

--- util.h
+++ util.h
@@ -6,14 +6,14 @@
 #include <linux/list.h>
 #include <linux/fs.h>
 
-#define __DEBUG__ 1             // General debugging statements
+#define __DEBUG__ 1		// General debugging statements
 #define __DEBUG_HOOK__ 1
 
-char *strnstr ( const char *haystack, const char *needle, size_t n );
-void *memmem ( const void *haystack, size_t haystack_size, const void *needle, size_t needle_size );
-void *memstr ( const void *haystack, const char *needle, size_t size );
+char *strnstr(const char *haystack, const char *needle, size_t n);
+void *memmem(const void *haystack, size_t haystack_size, const void *needle, size_t needle_size);
+void *memstr(const void *haystack, const char *needle, size_t size);
 
-void hijack_start ( void *target, void *new );
-void hijack_pause ( void *target );
-void hijack_resume ( void *target );
-void hijack_stop ( void *target );
+void hijack_start(void *target, void *new);
+void hijack_pause(void *target);
+void hijack_resume(void *target);
+void hijack_stop(void *target);
