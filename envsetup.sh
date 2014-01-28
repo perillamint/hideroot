af56f29c6af1ded1d9b873f5fd486100b06a4684
maneulyori <maneulyori@gmail.com>
2014-01-29 02:30:09 +0900

    envsetup for jenkins

---
+++ envsetup.sh
@@ -0,0 +1,4 @@
+export O=$WORKSPACE/../carbon-kk/out/target/product/e975/obj/KERNEL_OBJ/
+export KDIR=$WORKSPACE/../carbon-kk/kernel/lge/gproj
+export CROSS_COMPILE=$WORKSPACE/../carbon-kk/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
+export ARCH=arm
